ee05f1e42d1a8bed338ab5b5f19d42153b6a029c
user <user@408-07.kpc408.ru>
2021-12-21 09:02:52 +0300

    добавлено

---
+++ ConsoleApp2/ConsoleApp2/List.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace consoleapp
+{
+    public class List<Type>
+    {
+        private class ListItem
+        {
+            public Type Data;
+            public ListItem NextItem;
+        }
+
+        private ListItem firstItem;
+        private int length;
+
+        public int Length 
+        {
+            get
+            {
+                return length;
+            }
+
+            set
+            {
+                length = value;
+            }
+
+        } 
+
+        public void Add(Type data)
+        {            
+            ListItem listItem = new ListItem { Data = data };
+
+            if (firstItem == null)
+            {
+                firstItem = listItem;
+                length = 1;
+                return;
+            }
+
+            ListItem prevItem = firstItem;
+
+            while (prevItem.NextItem != null)
+            {
+                prevItem = prevItem.NextItem;
+            }
+            
+            prevItem.NextItem = listItem;
+            length += 1;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+
+            ListItem deleteItem = firstItem;
+            
+            for (int i = 0; i < index - 1; i++)
+            {
+                deleteItem = deleteItem.NextItem;
+            }
+           
+            deleteItem.NextItem = deleteItem.NextItem.NextItem;
+
+            length--;
+        }
+        
+        private void ValidateIndex(int index)
+        {
+            if (firstItem == null)
+                throw new Exception("Список пуст :/");
+
+            if (index >= length || index < 0)
+                throw new Exception($"Нет элемента для индекса {index}");
+        }
+
+        private Type GetItem(int index)
+        {
+            ValidateIndex(index);
+
+            ListItem item = firstItem;
+            
+            for (int i = 0; i < index; i++)
+            {
+                item = item.NextItem;
+            }
+
+            return item.Data;
+        }
+         
+        public Type this[int index]
+        {
+            get
+            {
+                return GetItem(index);
+            }
+        } 
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace consoleapp
+{
+    class Program
+    {        
+        public static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            List<string> stringList = new List<string>();
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+                intList.Add(random.Next(-100, 100));
+
+            stringList.Add("Красный");
+            stringList.Add("Омлет");
+            stringList.Add("Забор");
+            stringList.Add("Доска");
+            stringList.Add("Кошка");
+
+            Console.WriteLine("Целые числа");
+
+            for (int i = 0; i < intList.Length; i++)
+                Console.Write($"{intList[i]} ");
+
+            Console.WriteLine("");
+            Console.WriteLine("Список целых чисел:");
+
+            for (int i = 0; i < stringList.Length; i++)
+                Console.Write($"{stringList[i]} ");
+
+            Console.WriteLine("");
+ 
+            try
+            {
+                intList.RemoveAt(random.Next(-1, 12));
+                stringList.RemoveAt(random.Next(-1, 6));
+
+                Console.WriteLine("Список целых чисел:");
+
+                for (int i = 0; i < intList.Length; i++)
+                    Console.Write($"{intList[i]} ");
+
+                Console.WriteLine("");
+                Console.WriteLine("Список целых чисел:");
+
+                for (int i = 0; i < stringList.Length; i++)
+                    Console.Write($"{stringList[i]} ");
+
+                Console.WriteLine("");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка при удалении: индекс вне допустимого диапазона");
+            }
+        }
+    }
+}
